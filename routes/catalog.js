827bb8610937685425bf14c4e9ddf5c3efefdbad
Alexander Pavlov <izzkander@gmail.com>
2022-06-10 18:14:54 +0300

    11 tasks

--- routes/catalog.js
+++ routes/catalog.js
@@ -97,30 +97,30 @@ router.get('/genres',
 );
 
 // МАРШРУТЫ BOOKINSTANCE
-router.get('/bookinstance/create', 
-  book_instance_controller.bookinstance_create_get
-);
-router.post('/bookinstance/create', 
-  book_instance_controller.bookinstance_create_post
-);
-router.get('/bookinstance/:id/delete', 
-  book_instance_controller.bookinstance_delete_get
-);
-router.post('/bookinstance/:id/delete',
-  book_instance_controller.bookinstance_delete_post
-);
-router.get('/bookinstance/:id/update', 
-  book_instance_controller.bookinstance_update_get
-);
-router.post('/bookinstance/:id/update',
-  book_instance_controller.bookinstance_update_post
-);
-router.get('/bookinstance/:id', 
-  book_instance_controller.bookinstance_detail
-);
-router.get('/bookinstances', 
-  book_instance_controller.bookinstance_list
-);
+
+router.get('/bookinstance/create', book_instance_controller.bookinstance_create_get);
+
+// POST request for creating BookInstance.
+router.post('/bookinstance/create', book_instance_controller.bookinstance_create_post);
+
+// GET request to delete BookInstance.
+router.get('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_get);
+
+// POST request to delete BookInstance.
+router.post('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_post);
+
+// GET request to update BookInstance.
+router.get('/bookinstance/:id/update', book_instance_controller.bookinstance_update_get);
+
+// POST request to update BookInstance.
+router.post('/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
+
+// GET request for one BookInstance.
+router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
+
+// GET request for list of all BookInstance.
+router.get('/bookinstances', book_instance_controller.bookinstance_list);
+
 
 // ЭКСПОРТ МАРШРУТОВ
 module.exports = router;
